a7802afe3d72c8ef7cb7253f05a983aaadcc37c7
Tony Z Snark <tonyzsnarkproton.me@proton.me>
2024-11-10 22:29:26 +1000

    feat: sentry integration

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,22 +1,29 @@
 "use client";
 
-interface ErrorProps {
-  error: Error;
-  reset: () => void;
-}
+import * as Sentry from "@sentry/nextjs";
+import Error from "next/error";
+import { useEffect } from "react";
 
-export default function GlobalErrorPage({ error, reset }: ErrorProps) {
+export default function GlobalError({ error }: { error: Error }) {
+  useEffect(() => {
+    Sentry.captureException(error);
+  }, [error]);
+
   return (
-    <section
-      id={"welcome"}
-      data-testid={"home"}
-      className="grid p-5 gap-9 col-content grid-cols-panels content-center items-center"
-    >
-      <div data-testid={`home.content`}>
-        <div className="grid gap-5">
-          <h1>Error</h1>
-        </div>
-      </div>
-    </section>
+    <html>
+      <body>
+        <section
+          id={"welcome"}
+          data-testid={"home"}
+          className="grid p-5 gap-9 col-content grid-cols-panels content-center items-center"
+        >
+          <div data-testid={`home.content`}>
+            <div className="grid gap-5">
+              <h1>Error</h1>
+            </div>
+          </div>
+        </section>
+      </body>
+    </html>
   );
 }
