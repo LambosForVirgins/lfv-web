237ea7e05ee5524a4080e1ad63a7ad9127ea39f0
Tony Z Snark <tonyzsnarkproton.me@proton.me>
2024-11-10 16:53:21 +1000

    feat: error pages

---
+++ src/app/error.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+interface ErrorProps {
+  error: Error;
+  reset: () => void;
+}
+
+export default function ErrorPage({ error, reset }: ErrorProps) {
+  return (
+    <section
+      id={"welcome"}
+      data-testid={"home"}
+      className="grid p-5 gap-9 col-content grid-cols-panels content-center items-center"
+    >
+      <div data-testid={`home.content`}>
+        <div className="grid gap-5">
+          <h1>Error</h1>
+        </div>
+      </div>
+    </section>
+  );
+}
